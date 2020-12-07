e85c6971a0ca53b129e62615b970d220fa97182e
tmoigne <71131533+theomoigne@users.noreply.github.com>
2020-12-07 13:51:21 +0100

    code2

--- tt.go
+++ tt.go
@@ -1,40 +1,196 @@
 package main
 
-
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+)
+
+var (
+	normal            = "\033[0m"
+	grave             = "\033[1m"
+	soulignerGrave    = "\033[21m"
+	fondBlanc         = "\033[7m"
+	colorBlack        = "\033[30m"
+	colorRedSombre    = "\033[31m"
+	colorGreenSombre  = "\033[32m"
+	colorYellowSombre = "\033[33m"
+	colorBlueSombre   = "\033[34m"
+	colorPurpleSombre = "\033[35m"
+	colorCyanSombre   = "\033[36m"
+	colorRed          = "\033[91m"
+	colorGreen        = "\033[92m"
+	colorYellow       = "\033[93m"
+	colorBlue         = "\033[94m"
+	colorRose         = "\033[95m"
+	colorCyan         = "\033[96m"
+	colorWhite        = "\033[97m"
+	fondNoir          = "\033[40m"
+	fondRouge         = "\033[41m"
+	fondVert          = "\033[42m"
+	fondJaune         = "\033[43m"
+	fondBleu          = "\033[44m"
+	fondViolet        = "\033[45m"
+	fondCyan          = "\033[46m"
+	fondGris          = "\033[100m"
+	fondRoseRouge     = "\033[101m"
+	fondRose          = "\033[105m"
+	fondBleuCiel      = "\033[106m"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		return
-	}
-	s := os.Args[1]
-	for _, v := range os.Args[2:] {
-		s += " " + v
-	}
-	result := ""
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("choisie ta police de text et couleur :")
+	scanner.Scan()
+	choix, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		if choix < 1 || choix > 4 {
+			fmt.Println("... Ca n'existe pas. >.>")
+	if choix == 1 {
+		fmt.Println("vous avez choise entre la polise stantard, shadow et thinkertoy : ")
+		scanner.Scan()
+		police, err := strconv.Atoi(scanner.Text())
+		if police == 1 {
+			s := os.Args[1]
+			for _, v := range os.Args[2:] {
+				s += " " + v
+			}
+			result := ""
 
-	for i := 0; i < 8; i++ {
+			for i := 0; i < 8; i++ {
 
-		for _, v := range s {
+				for _, v := range s {
 
-			result += ScanLine(1 + int(v-32)*9 + i)
+					result += ScanLine(1 + int(v-32)*9 + i)
 
+				}
+			}
 		}
-		fmt.Println(result)
-		result = ""
-	}
+		if police == 2 {
+			s := os.Args[1]
+			for _, v := range os.Args[2:] {
+				s += " " + v
+			}
+			result := ""
+
+			for i := 0; i < 8; i++ {
+
+				for _, v := range s {
+
+					result += ScanLine2(1 + int(v-32)*9 + i)
+
+				}
+			}
+		}
+		if police == 3 {
+			s := os.Args[1]
+			for _, v := range os.Args[2:] {
+				s += " " + v
+			}
+			result := ""
+
+			for i := 0; i < 8; i++ {
+
+				for _, v := range s {
+
+					result += ScanLine3(1 + int(v-32)*9 + i)
+
+				}
+			}
+		}
+
+
+		if err != nil {
+			if police < 1 || police > 4 {
+				fmt.Println("... Ca n'existe pas. >.>")
+			}
+			fmt.Println(err.Error())
+			os.Exit(0)
+		}
+
+
+		if choix == 2 {
+			fmt.Println("vous avez choise entre la polise stantard, shadow et thinkertoy : ")
+			scanner.Scan()
+			police, err := strconv.Atoi(scanner.Text())
+			if couleur == 1 {
+				result := ""
+
+				for i := 0; i < 8; i++ {
+
+					for _, v := range s {
+
+						result += ScanLine(1 + int(v-32)*9 + i)
+
+					}
+				}
+			}
+			if couleur == 2 {
+				s := os.Args[1]
+				for _, v := range os.Args[2:] {
+					s += " " + v
+				}
+				result := ""
+
+				for i := 0; i < 8; i++ {
+
+					for _, v := range s {
+
+						result += ScanLine2(1 + int(v-32)*9 + i)
+
+					}
+				}
+			}
+			if police == 3 {
+				s := os.Args[1]
+				for _, v := range os.Args[2:] {
+					s += " " + v
+				}
+				result := ""
+
+				for i := 0; i < 8; i++ {
+
+					for _, v := range s {
+
+						result += ScanLine3(1 + int(v-32)*9 + i)
+
+					}
+				}
+			}
+
+
+			if err != nil {
+				if police < 1 || police > 4 {
+					fmt.Println("... Ca n'existe pas. >.>")
+				}
+				fmt.Println(err.Error())
+				os.Exit(0)
+			}
+
+
+
+
+			if len(os.Args) == 1 {
+return
+}
+s := os.Args[1]
+for _, v := range os.Args[2:] {
+s += " " + v
+}
+result := ""
+
+fmt.Println(result)
+result = ""
+
+}
+
 }
 
 func ScanLine(num int) string {
 
-	police1, err := os.Open("standard.txt")
-	police2, err := os.Open("shadow.txt")
-	scanner1 := bufio.NewScanner(police1)
-	scanner2 := bufio.NewScanner(police2)
+	fileName, err := os.Open("standard.txt")
+	scanner := bufio.NewScanner(fileName)
 	checkLine := 0
 	printLine := ""
 
@@ -43,19 +199,10 @@ func ScanLine(num int) string {
 		os.Exit(0)
 	}
 
-	for scanner1.Scan() {
+	for scanner.Scan() {
 
 		if checkLine == num {
-			printLine = scanner1.Text()
-		}
-		checkLine++
-
-	}
-
-	for scanner2.Scan() {
-
-		if checkLine == num {
-			printLine = scanner2.Text()
+			printLine = scanner.Text()
 		}
 		checkLine++
 
@@ -63,4 +210,75 @@ func ScanLine(num int) string {
 
 	return printLine
 
+}
+
+func ScanLine2(num int) string {
+
+	fileName, err := os.Open("shadow.txt")
+	scanner := bufio.NewScanner(fileName)
+	checkLine := 0
+	printLine := ""
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	for scanner.Scan() {
+
+		if checkLine == num {
+			printLine = scanner.Text()
+		}
+		checkLine++
+
+	}
+
+	return printLine
+}
+
+func ScanLine3(num int) string {
+
+	fileName, err := os.Open("thinkertoy.txt")
+	scanner := bufio.NewScanner(fileName)
+	checkLine := 0
+	printLine := ""
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	for scanner.Scan() {
+
+		if checkLine == num {
+			printLine = scanner.Text()
+		}
+		checkLine++
+
+	}
+
+	return printLine
+
+}
+
+func color(result string) {
+	fmt.Println("choisie ta couleur :")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	couleur, err := strconv.Atoi(scanner.Text())
+	fmt.Println("Vous allez choisir la couleur")
+	if couleur == 1 {
+		fmt.Println(colorBlue + result)
+	}
+	if couleur == 2 {
+		fmt.Println(colorBlack)
+	}
+	if err != nil {
+		fmt.Println("... 1, 2 ou TROIS ! Pas de(s) lettre(s) !")
+		if couleur < 1 || couleur > 4 {
+			fmt.Println("... Ca n'existe pas. >.>")
+		}
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
 }
